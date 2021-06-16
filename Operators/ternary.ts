d6fde2df2360f40399d8555d416017dc90c5865c
Fiqry choerudin <fiqrychoerudin@student.telkomuniversity.ac.id>
2021-06-16 14:56:13 +0700

    add ts playgorund.

--- Operators/ternary.ts
+++ Operators/ternary.ts
@@ -13,3 +13,5 @@ const status3:any = 1
 const stat = status1 == 0 ? 'hijau' : (status2 != 0 ? 'red' : 'blue')
 // akan mencetak biru karena statment pertama tidak terpenuhi ( status1(null) == 0) akan melanjutkan ke statment ke 2 (status2(0) != 0 = false)
 console.log(stat)
+
+[Playground Link](https://www.typescriptlang.org/play?#code/MYewdgzgLgBGCuBbAjDAXHJAjApgJxgF4ZkAGAbgChRJYFEAmdTRXA4gVipuhYGZm9NkRgB2KtXC9QAGxDsWqAHwsmAfhgByABYBLAFYBDeJuYAKfjAA8imBs1ZdeE802J8h7ZoCUVAPR+MIYA1oZgMO5ghogADroyoeF6RvAwoXg4UWngACa6ELowUPgxmfB6MHkw0IZQkbCleFDRhjBmZDAqHN4AdJKQIDI4PXIA5may8t6U-bw1UPAQyGhhAJ4iCDIys7DziwwrYOvEpDvVzQsQfIfHJDM8uxcie0tEJ3ZaycamGGYvTABCd72DI5H5aLAyeA4HyUAJBRIRTLAHDNYIwRzONKGDJZeb1GCNZqIVpQXQ5EJFEplCoWF7IMybGTeN4wUgskJhJEyML6eBQRHBHDnWoEoUwJh-C77MzsmBAtkiABmhhkEBw0x4g2GYyltW8QA)
